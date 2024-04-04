7182f27ac21ac03b73a4a70a6c54afa6ca362d72
ssuyogshukla <149228433+ssuyogshukla@users.noreply.github.com>
2024-04-04 08:19:08 +0530

    Create geo.js

---
+++ geo.js
@@ -0,0 +1,17 @@
+
+//shortcode ..
+
+const y = document.getElementById("demo");
+
+function getLocation() {
+  try {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } catch {
+    x.innerHTML = err;
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
